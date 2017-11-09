fd534d2d794422a98fa901224053f27fec2d354c
Zack Piper <zack@chronosinteractive.com>
2017-11-08 19:20:03 -0700

    build mobile menu

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
-console.log('hello world');
+(function() {
+  const navOpenClass = 'nav--open'
+
+  const openNavButton = document.querySelector('.js-open-nav')
+  const closeNavButton = document.querySelector('.js-close-nav')
+  const nav = document.querySelector('.js-nav')
+
+  const initialize = () => {
+    addEventListeners()
+  }
+
+  const addEventListeners = () => {
+    openNavButton.addEventListener('click', openNav)
+    closeNavButton.addEventListener('click', closeNav)
+  }
+
+  const openNav = (e) => {
+    nav.classList.add(navOpenClass)
+  }
+
+  const closeNav = (e) => {
+    nav.classList.remove(navOpenClass)
+  }
+
+  initialize()
+})()
